0655a29c84e7e104972e198e7a5394f14b62a4d0
Abraralhazza <pealhazza9@gmail.com>
2023-12-18 13:57:12 +0300

    Initial Commit

---
+++ Team26/Team26App.swift
@@ -0,0 +1,17 @@
+//
+//  Team26App.swift
+//  Team26
+//
+//  Created by Abrar Alhazza on 05/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Team26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
